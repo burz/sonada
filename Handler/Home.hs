ea38d1e8ab9da21f3e11626c06da7331a2594c0a
Anthony Burzillo <burz@jhu.edu>
2014-07-01 02:04:10 -0400

    Change around colors

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -9,7 +9,7 @@ getHomeR = do
     let handlerName = "getHomeR" :: Text
         code = Nothing :: Maybe Text
     defaultLayout $ do
-        let _console = _console' code 20 20 60 70
+        let _console = _console' code 20 10 80 70
         aDomId <- newIdent
         setTitle "sonada"
         $(widgetFile "homepage")
