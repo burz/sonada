5b989055b37531255ae1ff1b3fced35254b6c933
Anthony Burzillo <burz@jhu.edu>
2014-07-01 01:25:36 -0400

    Start using messages

--- Handler/Partials.hs
+++ Handler/Partials.hs
@@ -6,7 +6,6 @@ import Import
 
 _console' :: Maybe Text -> Int -> Int -> Int -> Int -> Widget
 _console' code top left width height = $(widgetFile "partials/_console")
-    where deflt = "$(function () { debug(\"lol\"); });" :: Text
 
 _moduleList' :: [Entity Module] -> Widget
 _moduleList' modules = $(widgetFile "partials/_moduleList")
