69dfb4c40cfee233ec3b0d0daf03b7da515354cb
Anthony Burzillo <burz@jhu.edu>
2014-07-01 23:42:04 -0400

    Add save button

--- Handler/Partials.hs
+++ Handler/Partials.hs
@@ -7,6 +7,7 @@ module Handler.Partials
 import Model
 
 import Import
+import Text.Julius (rawJS)
 
 _console' :: Maybe Text -> Int -> Int -> Int -> Int -> Widget
 _console' code top left width height = $(widgetFile "partials/_console")
@@ -15,6 +16,9 @@ _moduleList' :: [Entity Module] -> Widget
 _moduleList' modules = $(widgetFile "partials/_moduleList")
 
 _moduleInterface' :: Widget -> Enctype -> Maybe Text -> Widget
-_moduleInterface' form enctype code = let _console = _console' code 20 10 80 70 in
+_moduleInterface' form enctype code = do
+    let _console = _console' code 20 10 80 70
+    messageRender' <- getMessageRender
+    let messageRender = rawJS . messageRender'
     $(widgetFile "partials/_moduleInterface")
 
