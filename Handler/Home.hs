718cc107ec351d43b213f4345afc4ad888caab80
Anthony Burzillo <burz@jhu.edu>
2014-07-02 02:15:09 -0400

    Send save with ajax

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,7 +1,6 @@
 module Handler.Home where
 
 import Handler.Partials
-import Forms.Module
 
 import Import
 
@@ -9,9 +8,8 @@ getHomeR :: Handler Html
 getHomeR = do
     let handlerName = "getHomeR" :: Text
         code = Nothing :: Maybe Text
-    (form, enctype) <- generateFormPost moduleForm
     defaultLayout $ do
-        let _moduleInterface = _moduleInterface' form enctype code
+        let _moduleInterface = _moduleInterface' code
         setTitle "sonada"
         $(widgetFile "homepage")
 
--- Handler/Partials.hs
+++ Handler/Partials.hs
@@ -7,7 +7,6 @@ module Handler.Partials
 import Model
 
 import Import
-import Text.Julius (rawJS)
 
 _console' :: Maybe Text -> Int -> Int -> Int -> Int -> Widget
 _console' code top left width height = $(widgetFile "partials/_console")
@@ -15,10 +14,8 @@ _console' code top left width height = $(widgetFile "partials/_console")
 _moduleList' :: [Entity Module] -> Widget
 _moduleList' modules = $(widgetFile "partials/_moduleList")
 
-_moduleInterface' :: Widget -> Enctype -> Maybe Text -> Widget
-_moduleInterface' form enctype code = do
+_moduleInterface' :: Maybe Text -> Widget
+_moduleInterface' code = do
     let _console = _console' code 20 10 80 70
-    messageRender' <- getMessageRender
-    let messageRender = rawJS . messageRender'
     $(widgetFile "partials/_moduleInterface")
 
