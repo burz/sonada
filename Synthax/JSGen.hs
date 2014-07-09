3ee91b6f7bdf5a40195f04dd932aa4350ff26ad3
Anthony Burzillo <burz@jhu.edu>
2014-07-09 03:17:55 -0400

    Move wrappers to thier own file

--- Synthax/JSGen.hs
+++ Synthax/JSGen.hs
@@ -6,12 +6,12 @@ module Synthax.JSGen
 import Synthax.Algebra
 import Synthax.AST
 import Synthax.Builders
+import Synthax.Wrappers
 
-import Prelude hiding (max)
+import Prelude
 import Control.Applicative
 import Control.Monad.State
 import Data.Monoid
-import Data.Text
 import Data.Text.Internal.Builder
 import Text.Julius
 
@@ -42,44 +42,6 @@ type JSAlgebra = MAlgebra HandleGenerator Expr (JSResult Builder)
 
 createJSLabel :: Int -> Builder
 createJSLabel i = "__varNum" .<>. show i
-
-sourceWrapper :: Builder -> Int -> Builder
-sourceWrapper label fileHandle = sid "var " .<> label
-    <>. sid " = SourceTool(__audioContext(), __bufferContext().getBuffer("
-    <>. show fileHandle <>. sid "), true);\n"
-
-codeWrapper :: Builder -> Text -> Builder
-codeWrapper label code = sid "var " .<> label
-    <>. sid " = __audioContext().createScriptProcessor(16384, 0, 1);\n"
-    <>. code <>. sid "\n"
-    <> label <>. sid ".onaudioprocess = function(audioProcessingEvent) {\n"
-    <>. sid "var outputBuffer = audioProcessingEvent.outputBuffer;\n"
-    <>. sid "var currentTime = audioContext.currentTime;\n"
-    <>. sid "for(var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {\n"
-    <>. sid "var outData = outputBuffer.getChannelData(channel);\n"
-    <>. sid "for(var sample = 0; sample < outputBuffer.length; sample++) {\n"
-    <>. sid "var sampleTime = currentTime +\n"
-    <>. sid "outputBuffer.duration *\n"
-    <>. sid "sample / outputBuffer.length;\n"
-    <>. sid "outData[sample] = gen(sampleTime);\n"
-    <>. sid "}\n" <>. sid "}\n" <>. sid "};\n"
-
-gainWrapper :: Builder -> Builder -> Float -> Builder
-gainWrapper label source value = sid "var " .<> label
-    <>. sid " = GainTool(__audioContext(), "
-    <> source <>. sid ", " <>. show value <>. sid ");\n"
-
-crossfadeWrapper :: Builder -> Builder -> Builder -> Float -> Float -> Builder
-crossfadeWrapper label source source' value max = sid "var " .<> label
-    <>. sid " = CrossfadeTool(__audioContext(), "
-    <> source <>. sid ", " <> source' <>. sid ", " <>. show value
-    <>. sid ", " <>. show max <>. sid ");\n"
-
-filterWrapper :: Builder -> Builder -> Text -> Float -> Builder
-filterWrapper label source typ frequency = sid "var " .<> label
-    <>. sid " = FilterTool(__audioContext(), "
-    <> source <>. sid ", " <>. typ <>. sid ", " <>. show frequency
-    <>. sid ");\n"
 
 alg :: JSAlgebra
 alg (Source f) = (\(n, h) -> let l = createJSLabel n in
---
+++ Synthax/Wrappers.hs
@@ -0,0 +1,53 @@
+module Synthax.Wrappers
+( sourceWrapper
+, codeWrapper
+, gainWrapper
+, crossfadeWrapper
+, filterWrapper
+) where
+
+import Synthax.Builders
+
+import Prelude
+import Data.Monoid
+import Data.Text
+import Data.Text.Internal.Builder
+
+sourceWrapper :: Builder -> Int -> Builder
+sourceWrapper label fileHandle = sid "var " .<> label
+    <>. sid " = SourceTool(__audioContext(), __bufferContext().getBuffer("
+    <>. show fileHandle <>. sid "), true);\n"
+
+codeWrapper :: Builder -> Text -> Builder
+codeWrapper label code = sid "var " .<> label
+    <>. sid " = __audioContext().createScriptProcessor(16384, 0, 1);\n"
+    <>. code <>. sid "\n"
+    <> label <>. sid ".onaudioprocess = function(audioProcessingEvent) {\n"
+    <>. sid "var outputBuffer = audioProcessingEvent.outputBuffer;\n"
+    <>. sid "var currentTime = audioContext.currentTime;\n"
+    <>. sid "for(var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {\n"
+    <>. sid "var outData = outputBuffer.getChannelData(channel);\n"
+    <>. sid "for(var sample = 0; sample < outputBuffer.length; sample++) {\n"
+    <>. sid "var sampleTime = currentTime +\n"
+    <>. sid "outputBuffer.duration *\n"
+    <>. sid "sample / outputBuffer.length;\n"
+    <>. sid "outData[sample] = gen(sampleTime);\n"
+    <>. sid "}\n" <>. sid "}\n" <>. sid "};\n"
+
+gainWrapper :: Builder -> Builder -> Float -> Builder
+gainWrapper label source value = sid "var " .<> label
+    <>. sid " = GainTool(__audioContext(), "
+    <> source <>. sid ", " <>. show value <>. sid ");\n"
+
+crossfadeWrapper :: Builder -> Builder -> Builder -> Float -> Float -> Builder
+crossfadeWrapper label source source' value max = sid "var " .<> label
+    <>. sid " = CrossfadeTool(__audioContext(), "
+    <> source <>. sid ", " <> source' <>. sid ", " <>. show value
+    <>. sid ", " <>. show max <>. sid ");\n"
+
+filterWrapper :: Builder -> Builder -> Text -> Float -> Builder
+filterWrapper label source typ frequency = sid "var " .<> label
+    <>. sid " = FilterTool(__audioContext(), "
+    <> source <>. sid ", " <>. typ <>. sid ", " <>. show frequency
+    <>. sid ");\n"
+
