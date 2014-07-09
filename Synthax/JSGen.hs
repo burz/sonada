1b31486599da1a207f07d49da5b3319a70960978
Anthony Burzillo <burz@jhu.edu>
2014-07-09 01:28:15 -0400

    Flesh out jsGen

--- Synthax/JSGen.hs
+++ Synthax/JSGen.hs
@@ -7,34 +7,40 @@ import Synthax.Algebra
 import Synthax.AST
 
 import Prelude
-import Text.Julius
+import Control.Applicative
+import Control.Monad.State
 import Data.Monoid
-import Data.Text
+import Data.Text.Internal.Builder
+import Text.Julius
 
-data JSResult a = JSResult Javascript a
-
-genRawFromText :: Text -> RawJavascript
-genRawFromText = rawJS
-
-genFromText :: Text -> Javascript
-genFromText = toJavascript . rawJS
+data JSResult a = JSResult [Builder] Builder a
 
 instance Monad JSResult where
-    return a = JSResult (genFromText "") a
-    JSResult t a >>= f = let JSResult t' b = f a in JSResult (mappend t t') b
+    return a = JSResult [] (fromText "")  a
+    JSResult fs t a >>= f
+        = let JSResult fs' t' b = f a in JSResult (fs ++ fs') (t <> t') b
 
-type JSAlgebra = MAlgebra JSResult Expr RawJavascript
+type HandleGenerator = State Int
+
+noLabel :: HandleGenerator ()
+noLabel = state (\i -> ((), i))
+
+newLabel :: HandleGenerator Int
+newLabel = state (\i -> (i, i + 1))
+
+type JSAlgebra = MAlgebra HandleGenerator Expr (JSResult Builder)
+
+createJSLabel :: Int -> Builder
+createJSLabel i = mappend (fromText "__varNum") . fromString $ show i
 
 alg :: JSAlgebra
-alg _ = JSResult (genFromText "") (genRawFromText "")
+alg (Source f) = (\n -> let l = createJSLabel n in
+    JSResult [fromText f] (fromText "") l) <$> newLabel
+alg _ = (\_ -> JSResult [] (fromText "") (fromText "")) <$> noLabel
 
-jsGen' :: Fix Expr -> JSResult RawJavascript
+jsGen' :: Fix Expr -> HandleGenerator (JSResult Builder)
 jsGen' e = mcata alg e
 
-instance ToJavascript Javascript where
-    toJavascript = id
-
-jsGen :: Fix Expr -> JavascriptUrl a
-jsGen e = let JSResult j n = jsGen' e in do
-    [julius|#{j} lololololol #{n}|]
+jsGen :: Fix Expr -> Javascript
+jsGen e = let JSResult fs j n = evalState (jsGen' e) 0 in Javascript j
 
