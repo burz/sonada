0ee845b5093d3409d1200b2df916a2bf9b1a4754
Anthony Burzillo <burz@jhu.edu>
2014-07-09 03:11:26 -0400

    Finish code wrappers

---
+++ Synthax/Builders.hs
@@ -0,0 +1,32 @@
+module Synthax.Builders
+( BuilderMappend(..)
+, sid
+, tid
+) where
+
+import Prelude
+import Data.Monoid
+import Data.Text
+import Data.Text.Internal.Builder
+
+class BuilderMappend a where
+    (.<>) :: a -> Builder -> Builder
+    (<>.) :: Builder -> a -> Builder
+    (.<>.) :: a -> a -> Builder
+
+instance BuilderMappend String where
+    s .<> b = fromString s <> b
+    b <>. s = b <> fromString s
+    s .<>. s' = fromString s <> fromString s'
+
+instance BuilderMappend Text where
+    t .<> b = fromText t <> b
+    b <>. t = b <> fromText t
+    t .<>. t' = fromText t <> fromText t'
+
+sid :: String -> String
+sid = id
+
+tid :: Text -> Text
+tid = id
+
--- Synthax/JSGen.hs
+++ Synthax/JSGen.hs
@@ -5,42 +5,98 @@ module Synthax.JSGen
 
 import Synthax.Algebra
 import Synthax.AST
+import Synthax.Builders
 
-import Prelude
+import Prelude hiding (max)
 import Control.Applicative
 import Control.Monad.State
 import Data.Monoid
+import Data.Text
 import Data.Text.Internal.Builder
 import Text.Julius
 
-data JSResult a = JSResult [Builder] Builder a
+type BufferFile = Builder
+
+data JSResult a = JSResult [(Int, BufferFile)] Builder a
 
 instance Monad JSResult where
     return a = JSResult [] (fromText "")  a
     JSResult fs t a >>= f
         = let JSResult fs' t' b = f a in JSResult (fs ++ fs') (t <> t') b
 
-type HandleGenerator = State Int
+type HandleGenerator = State (Int, Int)
 
 noLabel :: HandleGenerator ()
-noLabel = state (\i -> ((), i))
+noLabel = state (\(i, j) -> ((), (i, j)))
 
 newLabel :: HandleGenerator Int
-newLabel = state (\i -> (i, i + 1))
+newLabel = state (\(i, j) -> (i, (i + 1, j)))
+
+newFileHandle :: HandleGenerator Int
+newFileHandle = state (\(i, j) -> (j, (i, j + 1)))
+
+bothNew :: HandleGenerator (Int, Int)
+bothNew = state (\(i, j) -> ((i, j), (i + 1, j + 1)))
 
 type JSAlgebra = MAlgebra HandleGenerator Expr (JSResult Builder)
 
 createJSLabel :: Int -> Builder
-createJSLabel i = mappend (fromText "__varNum") . fromString $ show i
+createJSLabel i = "__varNum" .<>. show i
+
+sourceWrapper :: Builder -> Int -> Builder
+sourceWrapper label fileHandle = sid "var " .<> label
+    <>. sid " = SourceTool(__audioContext(), __bufferContext().getBuffer("
+    <>. show fileHandle <>. sid "), true);\n"
+
+codeWrapper :: Builder -> Text -> Builder
+codeWrapper label code = sid "var " .<> label
+    <>. sid " = __audioContext().createScriptProcessor(16384, 0, 1);\n"
+    <>. code <>. sid "\n"
+    <> label <>. sid ".onaudioprocess = function(audioProcessingEvent) {\n"
+    <>. sid "var outputBuffer = audioProcessingEvent.outputBuffer;\n"
+    <>. sid "var currentTime = audioContext.currentTime;\n"
+    <>. sid "for(var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {\n"
+    <>. sid "var outData = outputBuffer.getChannelData(channel);\n"
+    <>. sid "for(var sample = 0; sample < outputBuffer.length; sample++) {\n"
+    <>. sid "var sampleTime = currentTime +\n"
+    <>. sid "outputBuffer.duration *\n"
+    <>. sid "sample / outputBuffer.length;\n"
+    <>. sid "outData[sample] = gen(sampleTime);\n"
+    <>. sid "}\n" <>. sid "}\n" <>. sid "};\n"
+
+gainWrapper :: Builder -> Builder -> Float -> Builder
+gainWrapper label source value = sid "var " .<> label
+    <>. sid " = GainTool(__audioContext(), "
+    <> source <>. sid ", " <>. show value <>. sid ");\n"
+
+crossfadeWrapper :: Builder -> Builder -> Builder -> Float -> Float -> Builder
+crossfadeWrapper label source source' value max = sid "var " .<> label
+    <>. sid " = CrossfadeTool(__audioContext(), "
+    <> source <>. sid ", " <> source' <>. sid ", " <>. show value
+    <>. sid ", " <>. show max <>. sid ");\n"
+
+filterWrapper :: Builder -> Builder -> Text -> Float -> Builder
+filterWrapper label source typ frequency = sid "var " .<> label
+    <>. sid " = FilterTool(__audioContext(), "
+    <> source <>. sid ", " <>. typ <>. sid ", " <>. show frequency
+    <>. sid ");\n"
 
 alg :: JSAlgebra
-alg (Source f) = (\n -> let l = createJSLabel n in
-    JSResult [fromText f] (fromText "") l) <$> newLabel
-alg _ = (\_ -> JSResult [] (fromText "") (fromText "")) <$> noLabel
+alg (Source f) = (\(n, h) -> let l = createJSLabel n in
+    JSResult [(h, fromText f)] (sourceWrapper l h) l) <$> bothNew
+alg (Code t) = (\n -> let l = createJSLabel n in
+    JSResult [] (codeWrapper l t) l) <$> newLabel
+alg (Gain c v) = (\n j -> let l = createJSLabel n in
+    j >>= \s -> JSResult [] (gainWrapper l s v) l) <$> newLabel <*> c
+alg (Crossfade c c' v m) = (\n j j' -> let l = createJSLabel n in
+    j >>= \s -> j' >>= \s' -> JSResult [] (crossfadeWrapper l s s' v m) l)
+    <$> newLabel <*> c <*> c'
+alg (Filter c t f) = (\n j -> let l = createJSLabel n in
+    j >>= \s -> JSResult [] (filterWrapper l s t f) l) <$> newLabel <*> c
 
 jsGen' :: Fix Expr -> HandleGenerator (JSResult Builder)
 jsGen' e = mcata alg e
 
 jsGen :: Fix Expr -> Javascript
-jsGen e = let JSResult fs j n = evalState (jsGen' e) 0 in Javascript j
+jsGen e = let JSResult fs j n = evalState (jsGen' e) (0, 0) in Javascript j
 
