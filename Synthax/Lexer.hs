7aa5c8d702487fe703add6e3f037e09a0e5b99ee
Anthony Burzillo <burz@jhu.edu>
2014-07-10 01:38:05 -0400

    Add types to lexer

--- Synthax/Lexer.hs
+++ Synthax/Lexer.hs
@@ -13,24 +13,40 @@ import Prelude
 import Text.Parsec
 import qualified Text.Parsec.Token as Token
 import Text.Parsec.Language
+import Text.Parsec.String
 
+names :: [String]
 names = words "Source Code Gain Crossfade Filter"
+
+opNames :: [String]
 opNames = words "<<< >>>"
 
+lexer :: Token.TokenParser a
 lexer = Token.makeTokenParser emptyDef
     { Token.commentStart = "/*"
     , Token.commentEnd = "*/"
     , Token.commentLine = "#" 
     , Token.identStart = letter
-    , Token.identLetter = alphaNum <|> char '_' <|> char '\''
+    , Token.identLetter = letter <|> char '_'
     , Token.reservedNames = names
     , Token.reservedOpNames = opNames
     }
 
+identifier :: Parser String
 identifier = Token.identifier lexer
-symbol     = Token.symbol lexer
+
+symbol :: String -> Parser String
+symbol = Token.symbol lexer
+
+reserved :: String -> Parser ()
 reserved   = Token.reserved lexer
+
+reservedOp :: String -> Parser ()
 reservedOp = Token.reservedOp lexer
-parens     = Token.parens lexer
-float      = Token.float lexer
+
+parens :: Parser a -> Parser a
+parens = Token.parens lexer
+
+float :: Parser Double
+float = Token.float lexer
 
